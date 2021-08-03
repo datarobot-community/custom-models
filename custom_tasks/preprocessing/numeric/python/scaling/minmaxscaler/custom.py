f37eb84f04cb6f596e93a727f65914d78aef795c
Jameson Stillwell <jamesonstillwell@gmail.com>
2021-08-03 16:25:18 -0400

    black and isort

--- custom_tasks/preprocessing/numeric/python/scaling/minmaxscaler/custom.py
+++ custom_tasks/preprocessing/numeric/python/scaling/minmaxscaler/custom.py
@@ -1,12 +1,14 @@
-import pandas as pd
-from pathlib import Path
-import pickle
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
 import os
+import pickle
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
 
 def fit(X, y, output_dir, **kwargs):
-    """ This hook defines how DataRobot will train this task. Even transform tasks need to be trained to learn/store information from training data
+    """This hook defines how DataRobot will train this task. Even transform tasks need to be trained to learn/store information from training data
     DataRobot runs this hook when the task is being trained inside a blueprint.
     As an output, this hook is expected to create an artifact containg a trained object, that is then used to transform new data.
     The input parameters are passed by DataRobot based on project and blueprint configuration.
@@ -32,17 +34,18 @@ def fit(X, y, output_dir, **kwargs):
     scaler.fit(X.values)
 
     # Dump fit scaler to artifact to use for transforms
-    with open(os.path.join(output_dir, 'minmaxscaler.pkl'), "wb") as fp:
+    with open(os.path.join(output_dir, "minmaxscaler.pkl"), "wb") as fp:
         pickle.dump(scaler, fp)
 
     # Save the transformed input df as an object to inspect and confirm the scaler is working
     transformed = pd.DataFrame(scaler.transform(X.values))
 
-    transformed.to_csv(os.path.join(output_dir, "transformed.csv"), index = False)
+    transformed.to_csv(os.path.join(output_dir, "transformed.csv"), index=False)
 
-def transform(data, transformer): 
-    """ This hook defines how DataRobot will use the trained object from fit() to transform new data.
-    DataRobot runs this hook when the task is used for scoring inside a blueprint. 
+
+def transform(data, transformer):
+    """This hook defines how DataRobot will use the trained object from fit() to transform new data.
+    DataRobot runs this hook when the task is used for scoring inside a blueprint.
     As an output, this hook is expected to return the transformed data.
     The input parameters are passed by DataRobot based on dataset and blueprint configuration.
 
@@ -52,14 +55,15 @@ def transform(data, transformer):
         Data that DataRobot passes for transformation.
     transformer: Any
         Trained object, extracted by DataRobot from the artifact created inside fit().
-    
+
     Returns
     -------
     pd.DataFrame
         Returns a dataframe with transformed data.
     """
 
-    transformed = pd.DataFrame(transformer.transform(data.values), columns=data.columns).fillna(np.nan)
+    transformed = pd.DataFrame(
+        transformer.transform(data.values), columns=data.columns
+    ).fillna(np.nan)
 
     return transformed
-
